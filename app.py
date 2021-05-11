ca6b57ff2b0128105d4bfc4bffebeb8e77df0351
Ruoyu Jiang <ruoyujiang@Ruoyus-MacBook-Pro.local>
2021-05-11 23:50:53 +0300

    Try to train

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
 from flask import Flask
+from flask import request
+
+from chatterbot import ChatBot
+
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    content = request.json
+
+    print("Ask: ", content.get("content"))
+
+    answer = bot.get_response(content.get("content"))
+
+    print("Answer: ", answer)
+
+    return str(answer)
---
+++ train.py
@@ -0,0 +1,92 @@
+from app import bot
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+trainer.train([
+    'How are you?',
+    'I am your dad, do you know that?',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
